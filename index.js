d8fef334f0c917b774bf3d3e19e6b2f531a78228
Liziyeah <marianitalizcanor@gmail.com>
2024-02-09 17:52:34 -0500

    quiz

---
+++ index.js
@@ -0,0 +1,12 @@
+const studentGrades = [100, 20, 70, 89, 60];
+
+const retornarInfo = (studentGrades) => {
+	objetoNotas = [];
+	for (let i = 0; i < studentGrades.length; i++) {
+		if (studentGrades[i] > 90) {
+			let a;
+		}
+	}
+};
+
+const resultado = retornarInfo(studentGrades);
